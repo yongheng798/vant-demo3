97346568d0516f0a49548d803d66fb694a865418
yongheng798 <1351817260@qq.com>
2020-07-20 16:25:58 +0800

    fix: 条件进度条组件失败再次提交

---
+++ src/components/vant/steps/StepsIndex.vue
@@ -0,0 +1,45 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-07-20 15:36:16
+ * @LastEditTime: 2020-07-20 16:14:18
+ * @LastEditors: Please set LastEditors
+ * @Description: 步骤条
+ * @FilePath: \vant-demo3\src\components\vant\steps\index.vue
+-->
+<template>
+  <div class="steps-wrap-box">
+    <van-steps :active="active" @click-step="handleClickStep">
+      <van-step>买家下单</van-step>
+      <van-step>商家接单</van-step>
+      <van-step>买家提货</van-step>
+      <van-step>交易完成</van-step>
+    </van-steps>
+  </div>
+</template>
+
+<script>
+import { Step, Steps } from 'vant'
+export default {
+  name: 'Steps',
+  components: {
+    [Step.name]: Step,
+    [Steps.name]: Steps
+  },
+  data() {
+    return {
+      active: 0
+    }
+  },
+  methods: {
+    handleClickStep(index) {
+      this.active = index
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.steps-wrap-box{
+  padding: 10px;
+}
+</style>
