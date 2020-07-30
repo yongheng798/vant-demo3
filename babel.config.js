064aec048aa60a007f83dc455774cfaf3d715cb9
yongheng798 <1351817260@qq.com>
2020-07-30 09:46:49 +0800

    mod: 新增打包去除console注释

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,21 @@
 /*
  * @Author: your name
  * @Date: 2020-07-07 20:38:01
- * @LastEditTime: 2020-07-09 16:43:03
+ * @LastEditTime: 2020-07-30 08:46:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vant-demo3\babel.config.js
  */
+let transformRemoveConsolePlugin = []
+if (process.env.NODE_ENV === 'production') {
+  transformRemoveConsolePlugin = ['transform-remove-console']
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset', '@babel/preset-env'
   ],
   plugins: [
+    ...transformRemoveConsolePlugin,
     ['import', {
       libraryName: 'vant',
       libraryDirectory: 'es',
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
+<!--
+ * @Descripttion: 陈品富写的demo描述
+ * @version:
+ * @Author: chenpinfu~陈品富
+ * @Date: 2020-07-19 12:14:32
+ * @LastEditors: chenpinfu~陈品富
+ * @LastEditTime: 2020-07-30 09:13:58
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page{{ msg }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      msg: 'hello world'
+    }
+  },
+  methods: {
+  /**
+   * @name:测试方法检测
+   * @test: test font
+   * @msg:
+   * @param {type}
+   * @return:
+   */
+  }
+}
+</script>
